36f637a19a69c59b1b2aeb8c513128eeca15ee3c
Bernard Mordan <bernardmordan@gmail.com>
2015-01-15 20:57:21 +0000

    finally not pushing secrets

--- server/accounts.js
+++ server/accounts.js
@@ -5,24 +5,24 @@ Meteor.startup(function(){
   })
   Accounts.loginServiceConfiguration.insert({
     service: "facebook",
-    appId: "1553595334886567",
-    secret: "ed88e95e8feaecdab5217ea605bbe3cf"
+    appId: accounts.facebookAppId,
+    secret: accounts.facebookSecret
   })
   Accounts.loginServiceConfiguration.remove({
     service: "twitter"
   })
   Accounts.loginServiceConfiguration.insert({
     service: "twitter",
-    consumerKey: "H6hkZgRLkSdwt4OrgeuGttUrf",
-    secret: "ku7b9QtFIBWfuPO92Ho0G4Ba8Xth7UiVD63yDcjadyeHFQopel"
+    consumerKey: accounts.twitterConsumerKey,
+    secret: accounts.twitterSecret
   })
   Accounts.loginServiceConfiguration.remove({
     service: "google"
   })
   Accounts.loginServiceConfiguration.insert({
     service: "google",
-    clientId: "769803526244-vtp8cjfhbn7u459oomdkghria2n5ic5d.apps.googleusercontent.com",
-    secret: "-tJ0dVmLrCbavnlQc_K3LY0_"
+    clientId: accounts.googleClientId,
+    secret: accounts.googleSecret
   })
 
 })
